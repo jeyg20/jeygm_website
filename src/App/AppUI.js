dbaa74d22b03db6f723eef0f0dd72c5488e241f7
Jeison <jei_son1516@hotmail.com>
2022-08-01 06:44:46 -0400

    CustomLink function created to handle naviation bar href to the pages

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,33 +1,47 @@
 import React from "react";
 import './App.css';
 import { Home } from "../Home";
+import { Proyects } from "../Proyects";
 
 function AppUI () {
+    let Component
+    switch (window.location.pathname) {
+        case "/Home":
+            Component = Home;
+            break;
+        case "/Proyects":
+            Component = Proyects;
+            break;
+        default:
+            Component = Home
+            break;
+    }
     return (
         <React.Fragment>
             <header>
                 <nav class="nav-bar">
                     <ul class="nav-bar-items">
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">Proyects</a>
-                        </li>
-                        <li>
-                            <a href="#">Blog</a>
-                        </li>
-                        <li>
-                            <a href="#">About</a>
-                        </li>
+                        <CustomLink href="/Home">Home</CustomLink>
+                        <CustomLink href="/Proyects">Proyects</CustomLink>
+                        <CustomLink href="#">Blog</CustomLink>
+                        <CustomLink href="#">About</CustomLink>
                     </ul>
                 </nav>
             </header>
             <main>
-                <Home/>
+                <Component/>
             </main>
         </React.Fragment>
     );
+}
+
+function CustomLink({ href, children, ...props}) {
+    const path = window.location.pathname;
+    return (
+        <li className={path === href ? "active" : ""}>
+            <a href={href} {...props} >{children}</a>
+        </li>
+    )
 }
 
 export { AppUI };
