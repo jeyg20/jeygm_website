7b16fd82b37b7d40875390977698f592bab3c576
Jeison <jei_son1516@hotmail.com>
2022-08-01 03:47:26 -0400

    navigation bar list items

---
+++ src/App/index.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <header>
+        <nav class="nav-bar">
+            <ul class="nav-bar-items">
+                <li>
+                    <a href="./index.html">Home</a>
+                </li>
+                <li>
+                    <a href="./proyects.html">Proyects</a>
+                </li>
+                <li>
+                    <a href="./blog.html">Blog</a>
+                </li>
+                <li>
+                    <a href="./about.html">About</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
+
+export default App;
